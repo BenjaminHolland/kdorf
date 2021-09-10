e9f906564029798503d5cb5e5c79801e5ac77c84
Ben Holland <ben.holland.core@gmail.com>
2021-09-10 11:08:22 -0700

    ramping up for firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.21"
     kotlin("kapt") version "1.5.21"
+    kotlin("plugin.serialization") version "1.5.21"
     application
 }
 
---
+++ src/main/kotlin/land/generic/kdorf/modules/chatting/FirebaseVocabSource.kt
@@ -0,0 +1,9 @@
+package land.generic.kdorf.modules.chatting
+
+import com.google.cloud.firestore.Firestore
+import javax.inject.Inject
+
+
+class FirebaseVocabSource @Inject constructor(private val firestore:Firestore) {
+
+}
