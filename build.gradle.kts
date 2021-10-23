3a07d0931ecce8a1fe8ae8190cf72fde7b09de64
Ben Holland <ben.holland.core@gmail.com>
2021-10-23 10:33:24 -0700

    Refactored some dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 
 plugins {
     kotlin("jvm") version "1.5.21"
@@ -14,17 +15,24 @@ repositories {
     mavenCentral()
     google()
 }
-
+fun DependencyHandlerScope.dagger(){
+    val version = "2.39.1"
+    implementation("com.google.dagger:dagger:$version")
+    kapt("com.google.dagger:dagger-compiler:$version")
+}
+fun DependencyHandlerScope.logging(){
+    val version = "2.14.1"
+    implementation("org.apache.logging.log4j:log4j-api:$version")
+    implementation("org.apache.logging.log4j:log4j-core:$version")
+    implementation("org.slf4j:slf4j-simple:1.7.32")
+}
 dependencies {
     implementation("dev.kord:kord-core:0.8.0-M4")
-    implementation("com.google.dagger:dagger:2.38.1")
-    kapt("com.google.dagger:dagger-compiler:2.38.1")
+    dagger()
+    logging()
     implementation(group = "com.uchuhimo", name = "konf", version = "1.1.2")
-    implementation("com.soywiz.korlibs.korim:korim-jvm:2.4.1")
-    implementation("com.google.cloud:google-cloud-firestore:3.0.3")
-    implementation("org.apache.logging.log4j:log4j-api:2.14.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
-    implementation("org.slf4j:slf4j-simple:1.6.1")
+    implementation("com.soywiz.korlibs.korim:korim-jvm:2.4.6")
+    implementation("com.google.cloud:google-cloud-firestore:3.0.5")
     testImplementation(kotlin("test"))
 }
 
@@ -34,7 +42,13 @@ tasks.test {
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
-
+tasks.jar{
+    manifest {
+        attributes(mapOf("Implementation-Title" to project.name,
+            "Implementation-Version" to project.version,
+            "Main-Class" to "MainKt"))
+    }
+}
 application {
     mainClassName = "MainKt"
 }
